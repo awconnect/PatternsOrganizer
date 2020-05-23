82eb00405710b0029a46fdb7d384a3493895d3c0
Abhi Wadekar <abhi1@berkeley.com>
2020-05-23 15:12:37 -0700

    added form input for expected time, in hours and minutes

--- src/form-data-component.js
+++ src/form-data-component.js
@@ -11,6 +11,9 @@ export default class FormDataComponent extends Component {
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangeDueDate = this.onChangeDueDate.bind(this);
         this.onChangeCompleted = this.onChangeCompleted.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeExpHours = this.onChangeExpHours.bind(this);
+        this.onChangeExpMins = this.onChangeExpMins.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
@@ -18,6 +21,9 @@ export default class FormDataComponent extends Component {
             email: '',
             dueDate: '',
             completed: false,
+            description: '',
+            expHours: '',
+            expMins: '',
         }
         
         if (JSON.parse(localStorage.getItem('default'))){
@@ -42,16 +48,24 @@ export default class FormDataComponent extends Component {
         this.setState({ email: e.target.value })
     }
 
-    onChangePhone(e) {
-        this.setState({ phone: e.target.value })
-    }
-
     onChangeDueDate(e) {
         this.setState({ dueDate: e.target.value })
     }
 
     onChangeCompleted(e) {
         this.setState({ completed: e.target.checked })
+    }
+
+    onChangeDescription(e) {
+        this.setState({ description: e.target.value })
+    }
+
+    onChangeExpHours(e) {
+        this.setState({ expHours: e.target.value })
+    }
+
+    onChangeExpMins(e) {
+        this.setState({ expMins: e.target.value })
     }
 
     onSubmit(e) {
@@ -64,9 +78,11 @@ export default class FormDataComponent extends Component {
         const taskItem = {
             name: this.state.name,
             email: this.state.email,
-            phone: this.state.phone,
             dueDate: this.state.dueDate,
-            completed: this.state.completed
+            completed: this.state.completed,
+            description: this.state.description,
+            expHours: this.state.expHours,
+            expMins: this.state.expMins,
         }
 
         localStorage.setItem('user' + this.state.number , JSON.stringify(taskItem));
@@ -75,9 +91,11 @@ export default class FormDataComponent extends Component {
         this.setState({
             name: '',
             email: '',
-            phone: '',
             dueDate: '',
-            completed: false
+            completed: false,
+            description: '',
+            expHours: '',
+            expMins: '',
         })
     }
 
@@ -89,17 +107,21 @@ export default class FormDataComponent extends Component {
             this.setState({
                 name: this.userData.name,
                 email: this.userData.email,
-                phone: this.userData.phone,
                 dueDate: this.userData.dueDate,
-                completed: this.userData.completed
+                completed: this.userData.completed,
+                description: this.userData.description,
+                expHours: this.userData.expHours,
+                expMins: this.userData.expMins,
             })
         } else {
             this.setState({
                 name: '',
                 email: '',
-                phone: '',
                 dueDate: '',
-                completed: false
+                completed: false,
+                description: '',
+                expHours: '',
+                expMins: '',
             })
         }
 
@@ -130,7 +152,24 @@ export default class FormDataComponent extends Component {
                         <label>Completed?</label>
                         <input type="checkbox" id="iscompleted" className="form-control" name="box1" checked={this.state.completed||false} onChange={this.onChangeCompleted}/>
                     </div>
+                    <div className="form-group"> 
+                        <label> Description </label>
+                        <textarea id="txtArea" className="form-control" rows="10" cols="70" name="txtbox" value={this.state.description || ''} onChange={this.onChangeDescription}/>
+                    </div>
+                    <div className="container-fluid"> 
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label>Hours</label>
+                                <input type="number" className="form-control" min="1" max="100" value={this.state.expHours || ''} onChange={this.onChangeExpHours}/>
+                            </div>
+                            <div class="col-lg-6">
+                                <label>Minutes</label>
+                                <input type="number" className="form-control" min="0" max="60" value={this.state.expMins || ''} onChange={this.onChangeExpMins}/>
+                            </div>
+                        </div>
+                    </div>
 
+                    <br />
                     <button type="submit" className="btn btn-primary btn-block">Submit</button>
                 </form>
             </div>
